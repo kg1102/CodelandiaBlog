cac65cdbc64eb124a526c9459d40e81283286988
0KaioGomes <kg6370694@gmail.com>
2021-07-02 20:42:40 -0300

    teste

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,7 +34,13 @@ export default function App(){
 
 
     useEffect(()=>{
-        axios.get('http://api.mediastack.com/v1/news?access_key=2918d6b36db943a130970d697aa6dfec&countries=br').then(response => {
+        axios.get('http://api.mediastack.com/v1/news?access_key=2918d6b36db943a130970d697aa6dfec&countries=br', {
+            headers: {
+                'referer': 'http://localhost',
+                'host':"localhost",
+                'origin': 'http://localhost/',
+            }
+        }).then(response => {
             setNews(response.data.data);
             setLoading(false);
             ScrollReveal().reveal('.body__card', { delay: 200, viewFactor: 0.8 });
