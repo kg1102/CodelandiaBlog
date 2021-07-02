4ba09a0e53b71f4d2d0c7b8e1a2737e7fc3a99d7
0KaioGomes <kg6370694@gmail.com>
2021-07-02 20:00:39 -0300

    Atualizações

--- public/index.js
+++
@@ -1,5 +0,0 @@
-const init = () => {
-    ScrollReveal().reveal('.body__card', { delay: 250, viewFactor: 0.7 });
-} 
-
-window.onload = init;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,24 @@ import { useHistory } from 'react-router-dom';
 import '../assets/styles/App.scss';
 import SearchImg from '../assets/images/search.svg';
 import LikeImg from '../assets/images/like.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import ScrollReveal from 'scrollreveal';
+import SyncLoader from "react-spinners/SyncLoader";
+
+
 
 export default function App(){
+
+    type newType = {
+        title: string,
+        description: string,
+        publishedAt: string,
+    }
+
+    const [textSearch, setTextSearch] = useState('');
+    const [news, setNews] = useState<newType[]>([]);
+    const [loading, setLoading] = useState(true);
 
     let history = useHistory();
 
@@ -16,8 +32,17 @@ export default function App(){
         history.push("/");
     }
 
+
+    useEffect(()=>{
+        axios.get('https://newsapi.org/v2/top-headlines?country=br&apiKey=9c24c765b67b48eab01b0dea9682e85f').then(response => {
+            setNews(response.data.articles);
+            setLoading(false);
+            ScrollReveal().reveal('.body__card', { delay: 200, viewFactor: 0.8 });
+        });
+    }, []);
+
     return (
-        <>
+        <>  
             <div className="header">
                 <div className="header__top">
                     <h3 onClick={handleReturnToHome}>Codelândia</h3>
@@ -27,49 +52,47 @@ export default function App(){
                     <div className="header__imgsearch">
                         <img src={SearchImg} alt="Search"/>
                     </div>
-                    <input className="header__input" placeholder="Pesquisar no blog"></input>
+                    <input 
+                        className="header__input" 
+                        placeholder="Pesquisar no blog"
+                        onChange={event => setTextSearch(event.target.value)}
+                        value={textSearch}
+                    />
                 </div>
             </div>
-            <div className="body__card">
-                <div className="body__header">
-                    <p>03 de jul, 2021</p>
-                    <img src={LikeImg} alt="like" onClick={handleLikeButton}/>
+
+            {loading 
+            
+            ?
+                <div className="loading">
+                    <SyncLoader color={"#CE153B"} loading={loading} size={16} />
+                    <h1>Carregando...</h1>
+                    <span>Uma pessoa que nunca cometeu um erro, nunca tentou nada novo.</span>
                 </div>
-                <br></br>
-                <div className="body__title">
-                    <h2>Windows 11 é oficialmente anunciado pela Microsoft</h2>
-                </div>
-                <div className="body__description">
-                    <p>A Microsoft lançou nesta quinta-feira (24) o aguardado Windows 11. A nova geração do sistema operacional para computadores já havia sido alvo de uma série de rumores e vazamentos nas últimas semanas. A build do SO, inclusive, ficou disponível para download por alguns dias, antes de a empresa derrubar as páginas.</p>
-                </div>
-            </div> 
-            <div className="body__card">
-                <div className="body__header">
-                    <p>02 de jul, 2021</p>
-                    <img src={LikeImg} alt="like" onClick={handleLikeButton}/>
-                </div>
-                <br></br>
-                <div className="body__title">
-                    <h2>Tim Berners-Lee vai leiloar código-fonte da web</h2>
-                </div>
-                <div className="body__description">
-                    <p>Código-fonte original da Web é vendido em NFT por quase R$ 27 milhões. O código original da Web foi vendido como um token não fungível (NFT) nesta quarta-feira (30). O cientista e físico britânico Tim Berners-Lee, também conhecido como “Pai da Web”, leiloou o documento por US$ 5,4 milhões — cerca de R$ 27 milhões.</p>
-                </div>
-            </div> 
-            <div className="body__card">
-                <div className="body__header">
-                    <p>02 de jul, 2021</p>
-                    <img src={LikeImg} alt="like" onClick={handleLikeButton}/>
-                </div>
-                <br></br>
-                <div className="body__title">
-                    <h2>John McAfee, criador do antivírus McAfee, morre</h2>
-                </div>
-                <div className="body__description">
-                    <p>O empresário John McAfee, de 75 anos, foi encontrado morto numa prisão em Barcelona. Figura polêmica, ele era investigado por fraude fiscal.</p>
-                </div>
-            </div>
-            <br></br><br></br><br></br><br></br><br></br>
+            :
+                <>
+                    {news.map(neww => {
+                        return (
+                            <div className="body__card">
+                                <div className="body__header">
+                                    <p>{neww.publishedAt}</p>
+                                    <img src={LikeImg} alt="like" onClick={handleLikeButton}/>
+                                </div>
+                                <br></br>
+                                <div className="body__title">
+                                    <h2>{neww.title}</h2>
+                                </div>
+                                <div className="body__description">
+                                    <p>{neww.description}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <div className="separator">Fim das notícias</div>
+                </>
+            }
+
+            <div className="space"/>
         </>
     );
 }
