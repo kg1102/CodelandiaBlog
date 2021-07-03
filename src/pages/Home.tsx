d6655779ae94236252579aaf1119fac944912d21
0KaioGomes <kg6370694@gmail.com>
2021-07-02 22:32:57 -0300

    atualização readme.md

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ScrollReveal from 'scrollreveal';
 import SyncLoader from "react-spinners/SyncLoader";
+import Modal from 'react-modal';
 
 
 
